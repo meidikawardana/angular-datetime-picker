d747f7bbabc8bb6842bc079327cf5545ff9b547a
Meidika Wardana <jdeep555.mw@gmail.com>
2021-12-16 12:44:23 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
+
+import {
+  OwlDateTimeModule,
+  OwlNativeDateTimeModule
+} from 'ng-pick-datetime';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    OwlDateTimeModule,
+    OwlNativeDateTimeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
